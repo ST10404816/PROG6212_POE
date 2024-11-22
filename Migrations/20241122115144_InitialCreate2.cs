5e6a199bf1a290ed0416e80b88167328dd177d3f
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-11-22 15:12:04 +0200

    Migrations

---
+++ Migrations/20241122115144_InitialCreate2.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Claims");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalNotes",
+                table: "Claims",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CellNumber",
+                table: "Claims",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Claims",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CellNumber",
+                table: "Claims");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Claims");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalNotes",
+                table: "Claims",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "Claims",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
