e8d583e182f99f02e0b5782065b063b8c9ac5576
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-11-22 15:08:30 +0200

    Views

---
+++ Views/Claim/Success.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace POE.Views.Claim
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
